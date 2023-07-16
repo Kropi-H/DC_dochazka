8103662404eac04661b2aa22e1c3be845e736f55
kropi-h <kropi-h@github.com>
2023-07-16 11:51:08 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-
+import sys
 #import library
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, render_template, json, redirect, url_for
 import gspread
 
 #Service client credential from oauth2client
 from oauth2client.service_account import ServiceAccountCredentials
+
 
 app = Flask(__name__)
 
@@ -21,12 +22,56 @@ credential = ServiceAccountCredentials.from_json_keyfile_name('startup_funding.j
 client = gspread.authorize(credential)
 
 #Now will can access our google sheets we call client.open on StartupName
-spread_sheet = client.open_by_key('19UBIonRYVjlAPu7nWfRUD69oFeUV32sUuk_LeJK-AyM')
-work_sheet = spread_sheet.worksheet('users')
+workers_tab = client.open_by_key('19UBIonRYVjlAPu7nWfRUD69oFeUV32sUuk_LeJK-AyM')
+workers_sheet = workers_tab.worksheet('users')
 
-@app.route('/', methods=['GET'])
+
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return jsonify(work_sheet.get_all_records())
+    user_data = workers_sheet.get_all_records()
+
+    if request.method == 'POST':
+
+        username = request.form['name'].strip()
+        password = request.form['password'].strip()
+
+        if (not username or not password) or (username == 'username' and password == 'password'): # If username or password is empty
+            return render_template('login.html', page_title='login') # Return to login page
+        else:
+            user = workers_sheet.find(username)
+            if not user:
+                return render_template('login.html', page_title='login')  # Return to login page
+            print(user, file=sys.stdout)
+            user_row = user.row
+
+            row = workers_sheet.row_values(user_row)
+            if password == row[1]:
+                return "yes"
+            else:
+                return render_template('login.html', page_title='login')
+    else:
+        return render_template('login.html', page_title='login')
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/register_new_user')
+def register_new_user():
+    pass
+
+@app.route('/new_attendence')
+def new_attendence():
+    pass
+
+@app.route('/attendence_overview')
+def attendence_overview():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
 
 if __name__=='__main__':
     app.run(debug=True)
