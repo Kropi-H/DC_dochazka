6bd34f843f1bb564d7cd2f27f9f60a1d6bd22ab8
Kropi-H <kropacek.h@gmail.com>
2023-07-22 16:54:06 +0200

    allow registration to the table

--- app.py
+++ app.py
@@ -73,20 +73,24 @@ def register_new_user():
     user = get_current_user()
     if not user:
         return redirect('/')
-    if request.method == 'GET':
-        return render_template('new_registration.html', page_title='Nová registrace')
+    if user and request.method == 'GET':
+        return render_template('new_registration.html', page_title='Nová registrace',user=session.get('user_name'),role = int(session.get('role')))
     if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['second_name']
-        password = request.form['password']
-        working_role = request.form['role']
-        email = request.form['email']
-        phone = request.form['phone']
-        row=first_name, password, working_role, first_name, last_name, email, phone
-        #
-        length_of_list = len(workers_sheet.get_all_values())
-        workers_sheet.insert_row(row,length_of_list+1)
-        return '{}'.format(workers_sheet.get_all_values())
+
+        first_name = request.form['first_name'].strip()
+        last_name = request.form['second_name'].strip()
+        password = request.form['password'].strip()
+        working_role = int(request.form['role'].strip())
+        email = request.form['email'].strip()
+        phone = int(request.form['phone'].strip())
+
+        length_of_list = len(workers_sheet.get_all_values()) # count of all rows in sheet
+        new_user_row = first_name, password, working_role, first_name, last_name, email, phone, length_of_list
+        workers_sheet.insert_row(new_user_row,length_of_list+1) # put data to the table on the bottom line
+        return render_template('new_user_confirmation.html', page_title='Nový Uživatel',
+                               new_user_data=new_user_row,
+                               user=session.get('user_name'),
+                               role = int(session.get('role')))
 
 
 
